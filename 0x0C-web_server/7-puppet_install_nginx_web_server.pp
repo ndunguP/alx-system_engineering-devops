b3ccb8f881b6bf97787b2bdf51fd0e0cb3280143
ndunguP <peter.ndunguoz50@gmail.com>
2023-06-28 12:15:01 -0700

    initial commit

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,7 +2,7 @@
 #a Bash script that configures a new Ubuntu machine to respect the follow requirements:
 #Install nginx on your web-01 server
 #Nginx should be listening on port 80
-#When querying Nginx at its root / with a GET request (requesting a page) using curl, it must return a page that contains the string Holberton School
+#When querying Nginx at its root / with a GET request (requesting a page) using curl, it must return a page that contains the string Hello World!
 sudo apt-get update
 sudo apt-get -y install nginx
 service nginx start
